8b15a11b59f7f472378b2849f7824688011b9420
Roman Butsko <roman.butko.999@gmail.com>
2022-06-07 23:06:02 +0300

    added 3 tasks

--- src/script.js
+++ src/script.js
@@ -189,3 +189,10 @@ function differgenceBtwNumsTwo(num) {
   return resultNum;
 }
 differgenceBtwNumsTwo(20);
+
+// 14. Write a JavaScript program to compute the absolute difference between a specified number and 19. Returns triple their absolute difference if the specified number is greater than 19.
+
+function differenceBtwTwoNum(n) {
+  return (n < 19) ? 19 - n : n - 19;
+}
+differenceBtwTwoNum(10);
