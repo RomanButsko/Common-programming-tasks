fedf6acbbab342073b41726f1487770821cb86a2
Roman Butsko <75183199+RomanButsko@users.noreply.github.com>
2022-07-02 23:38:50 +0300

    added task

--- src/fundamental_block.js
+++ src/fundamental_block.js
@@ -12,3 +12,26 @@ function compareTwoObject(first, second) {
 compareTwoObject({ name: 'Alexey', age: '20' }, { name: 'Alexey', age: '20' });
 
 // 102.  Write a JavaScript program to copy a string to the clipboard.
+
+async function copyTextToClipboard(text) {
+  try {
+    await navigator.clipboard.writeText(text);
+    console.log('Text copied to clipboard');
+  } catch (err) {
+    console.error('Error in copying text: ', err);
+  }
+}
+document.querySelector('.copyBtn').addEventListener('click', () => {
+  const text = document.querySelector('.copyStr').value;
+  copyTextToClipboard(text);
+});
+
+// 103. Write a JavaScript program to converts a comma-separated values (CSV) string to a 2D array.
+
+function conver2DArray(str) {
+  const result = [];
+  str.split(',').map((item) => result.push([item]));
+  return result;
+}
+
+conver2DArray('str, dds, fdf, hgygi');
